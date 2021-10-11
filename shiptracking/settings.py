febf59d9e23c009322b1550c281bc5e33091176a
les <shipsafe1978@gmail.com>
2021-10-11 14:25:23 +0100

    'tweakkl'

--- shiptracking/settings.py
+++ shiptracking/settings.py
@@ -169,7 +169,7 @@ EMAIL_HOST_PASSWORD = 'udoka12345'
 #SERVER_EMAIL = 'nwekelesley9@gmail.com'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 SITE_ID = 1
